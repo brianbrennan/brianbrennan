40db6fc4c02d758f02372debb03c2a693b5b2600
Brian Brennan <bbrennan@students.stonehill.edu>
2015-05-20 18:58:47 -0400

    Added start of responsiveness

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
-<?php require('/blog/wp-load.php'); ?>
+<?php 
+require('blog/wp-load.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,26 +10,30 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="components/js/nav.js"></script>
 	<script src="components/js/work.js"></script>
+	<script src="components/js/hover.js"></script>
 	<link rel="stylesheet" href="components/css/style.css">
 </head>
 <body>
 	<header class="light nav-down">
 		<div class="wrapper">
-		<nav>
-			<a href="index.php"><div class="logo">
-				
-			</div>
+			<nav>
+				<a href="index.php"> 
+					<div class="logoContain">
+						<div class="logo">
 
-			</a>
+						</div>
+					</div>
 
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="work.php">Work</a></li></a>
-				<li><a href="about.php">About</a></li></a>
-				<li><a href="blog">Blog</a></li></a>
-				<li class="contact"><a href="contact.php">Contact</a></li></a>
-			</ul>
-		</nav>
+				</a>
+
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="work.php">Work</a></li></a>
+					<li><a href="about.php">About</a></li></a>
+					<li><a href="blog">Blog</a></li></a>
+					<li class="contact"><a href="contact.php">Contact</a></li></a>
+				</ul>
+			</nav>
 		</div>
 	</header>
 	<div class="clear"></div>
--- index.php
+++ index.php
@@ -4,49 +4,49 @@
 
 </div>
 
-<div class="wrapper">
-	<div class="content">
+<div class="content">
 
-		<div class="workSection section">
+	<div class="workSection section">
 
-			<h2>Work</h2>
+		<h2><a href="work.php">Work</a></h2>
 
-			<div class="rule2"></div>
+		<div class="rule2"></div>
 
-			<div class="workPiece">
-				
-			</div>
-			<div class="workPiece">
-				
-			</div>
-			<div class="workPiece">
-				
-			</div>
-			<div class="clear"></div>
+		<div class="workPiece">
+
 		</div>
+		<div class="workPiece">
 
-
-		
-		<div class="aboutSection section">
-
-			<h2>About</h2>
-
-			<div class="rule2"></div>
-
-
-			<p>High Life Williamsburg synth kale chips American Apparel tofu messenger bag, next level irony pickled Brooklyn distillery. Street art synth whatever Truffaut gastropub shabby chic. Tumblr keytar hashtag, Kickstarter PBR Wes Anderson brunch stumptown VHS letterpress American Apparel 3 wolf moon. Direct trade +1 narwhal crucifix, PBR&B squid wolf. Keffiyeh photo booth meggings cred. Next level Pinterest scenester mixtape pork belly Godard. Leggings salvia tattooed cornhole selvage, fingerstache keytar 8-bit Bushwick Wes Anderson health goth Odd Future organic Vice.</p>
-
-
-			<div class="clear"></div>
 		</div>
+		<div class="workPiece">
 
-		<div class="blogSection section">
-
-			<h2>Blog</h2>
-
-			<div class="rule2"></div>
+		</div>
+		<div class="clear"></div>
+	</div>
 
 
+
+	<div class="aboutSection section">
+
+		<h2><a href="about.php">About</a></h2>
+
+		<div class="rule2"></div>
+
+
+		<p>High Life Williamsburg synth kale chips American Apparel tofu messenger bag, next level irony pickled Brooklyn distillery. Street art synth whatever Truffaut gastropub shabby chic. Tumblr keytar hashtag, Kickstarter PBR Wes Anderson brunch stumptown VHS letterpress American Apparel 3 wolf moon. Direct trade +1 narwhal crucifix, PBR&B squid wolf. Keffiyeh photo booth meggings cred. Next level Pinterest scenester mixtape pork belly Godard. Leggings salvia tattooed cornhole selvage, fingerstache keytar 8-bit Bushwick Wes Anderson health goth Odd Future organic Vice.</p>
+
+
+		<div class="clear"></div>
+	</div>
+
+	<div class="blogSection section">
+
+		<h2><a href="blog">Blog</a></h2>
+
+		<div class="rule2"></div>
+
+
+		<div class="posts">
 			<?php
 			$args = array( 'numberposts' => '3' );
 			$recent_posts = wp_get_recent_posts($args);
@@ -54,9 +54,9 @@
 				echo '<div class ="post"><a href="' . get_permalink($recent["ID"]) . '"><h3>' .   $recent["post_title"].' <span>' .$recent['post_date']. '</h3></a></div> ';
 			}
 			?>
-
-			<div class="clear"></div>
 		</div>
+
+		<div class="clear"></div>
 	</div>
 </div>
 <?php include 'footer.php' ?>
